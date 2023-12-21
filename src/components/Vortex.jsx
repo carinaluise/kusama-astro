2b5ae969066a9b9c63038c78897d6284e89c92d3
carinaluise <carinalrobinson@gmail.com>
2023-12-21 21:12:40 +0000

    add vortex animation

---
+++ src/components/Vortex.jsx
@@ -0,0 +1,28 @@
+import { motion, useScroll, useTransform } from 'framer-motion';
+
+const Vortex = () => {
+  const { scrollYProgress } = useScroll();
+
+  const meshAnimation = useTransform(
+    scrollYProgress,
+    [0.1, 0.2],
+    [50, 150],
+    {
+      clamp: false,
+    }
+  );
+
+  return (
+    <motion.section
+      style={{ y: meshAnimation }}
+      className="relative bottom-16 my-24"
+    >
+      <img
+        className="mx-auto"
+        src={`/images/vortex.svg`}
+      />
+    </motion.section>
+  );
+};
+
+export default Vortex;
