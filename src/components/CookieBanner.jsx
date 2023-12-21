2670e78c798f292a6c32ed6d12ba8645949d09b6
carinaluise <carinalrobinson@gmail.com>
2023-12-21 21:13:59 +0000

    add cookie banner, modal, overlay

---
+++ src/components/CookieBanner.jsx
@@ -0,0 +1,74 @@
+import { useState, useEffect } from 'react';
+import { useLocalStorage } from '../hooks/use-local-storage.js';
+
+import { AnimatePresence, motion } from 'framer-motion';
+import { CookieModal } from './CookieModal.jsx';
+
+import Button from './Button.jsx';
+
+const CookieBanner = () => {
+  const [value, setValue] = useLocalStorage('cookie-settings');
+  const [isCookieBannerVisible, setIsCookieBannerVisible] = useState(false);
+  const [isOpen, setIsOpen] = useState(false);
+
+  useEffect(() => {
+    setIsCookieBannerVisible(value === null);
+  }, [value]);
+
+  return (
+    <>
+      {isCookieBannerVisible && (
+        <AnimatePresence initial={false}>
+          <motion.div
+            className="pointer-events-none fixed inset-0 z-30 flex items-end px-4 sm:p-6"
+            initial={{ opacity: 0, translateY: '4rem' }}
+            animate={{ opacity: 1, translateY: 0 }}
+            exit={{ opacity: 0 }}
+            transition={{ duration: 0.4 }}
+            aria-live="assertive"
+          >
+            <div className="pointer-events-auto fixed inset-x-0 bottom-0 bg-black ring-1 ring-gray/50 px-8">
+              <div className="flex flex-col justify-between gap-x-8 gap-y-4 md:flex-row md:items-center">
+                <p className="mt-1 max-w-[1100px] text-sm font-normal opacity-70 ">This website uses Cookies.</p>
+                <div className="mt-2 flex items-center gap-x-2">
+                  <Button
+                    varient="small"
+                    onClick={() => {
+                      setValue({
+                        necessary: true,
+                        functional: true,
+                        performance: true,
+                        marketing: true,
+                      });
+                    }}
+                  >
+                    Accept All
+                  </Button>
+                  <Button
+                    varient="small"
+                    onClick={() => {
+                      setValue({
+                        necessary: true,
+                        functional: false,
+                        performance: false,
+                        marketing: false,
+                      });
+                    }}
+                  >
+                    Reject All
+                  </Button>
+                  <Button varient="small" onClick={() => setIsOpen(true)}>
+                    Manage
+                  </Button>
+                </div>
+              </div>
+            </div>
+          </motion.div>
+        </AnimatePresence>
+      )}
+      <CookieModal closeModal={() => setIsOpen(false)} isOpen={isOpen} />
+    </>
+  );
+};
+
+export { CookieBanner };
---
+++ src/components/Modal.jsx
@@ -0,0 +1,49 @@
+import { Fragment } from 'react';
+import { Dialog, Transition } from '@headlessui/react';
+
+const Modal = ({ isOpen, closeModal, children }) => {
+  return (
+    <Transition.Root
+      show={isOpen}
+      as={Fragment}
+    >
+      <Dialog
+        as="div"
+        className="relative z-[100]"
+        onClose={closeModal}
+      >
+        <Transition.Child
+          as={Fragment}
+          enter="ease-out duration-300"
+          enterFrom="opacity-0"
+          enterTo="opacity-100"
+          leave="ease-in duration-200"
+          leaveFrom="opacity-100"
+          leaveTo="opacity-0"
+        >
+          <div className="fixed inset-0 bg-black bg-opacity-75 transition-opacity" />
+        </Transition.Child>
+
+        <div className="fixed inset-0 z-[1000] w-screen overflow-y-auto">
+          <div className="flex min-h-full items-end justify-center text-center sm:items-center">
+            <Transition.Child
+              as={Fragment}
+              enter="ease-out duration-300"
+              enterFrom="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+              enterTo="opacity-100 translate-y-0 sm:scale-100"
+              leave="ease-in duration-200"
+              leaveFrom="opacity-100 translate-y-0 sm:scale-100"
+              leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+            >
+              <Dialog.Panel className="border-[1px] border-gray/50 relative transform overflow-hidden bg-black text-white px-4 pb-4 pt-5 text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-xl sm:p-16">
+                {children}
+              </Dialog.Panel>
+            </Transition.Child>
+          </div>
+        </div>
+      </Dialog>
+    </Transition.Root>
+  );
+};
+
+export default Modal;
